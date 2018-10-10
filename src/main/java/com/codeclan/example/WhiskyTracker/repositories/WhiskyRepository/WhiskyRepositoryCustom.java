b77e57b1ee037cd9762eac737404792bb193f82c
janeduncan <janeduncan@live.co.uk>
2018-10-10 23:06:22 +0100

    Adds methods to WhiskyRepositoryCustom interface

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryCustom.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import java.util.List;
 
 public interface WhiskyRepositoryCustom {
+
+    List<Whisky> findAllWhiskyByYear(int year);
+    List<Whisky> findAllWhiskyByRegion(String region);
+    List<Whisky> findAllWhiskyByDistilleryAndAge(Distillery distillery, int age);
+
 }
